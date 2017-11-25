0fa50ca4da65a27174acfc287b3a45193d9e9662
Darkjeff <jeff@jeffinfo.com>
2017-11-25 18:57:01 +0100

    Fix Bug: Erreur des Dictionnaires
    add 2 tables

---
+++ htdocs/custom/immobilier/sql/llx_c_immo_type_compteur.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Sam 25 Novembre 2017 à 18:55
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `jeffimmo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `llx_c_immo_type_compteur`
+--
+
+CREATE TABLE IF NOT EXISTS `llx_c_immo_type_compteur` (
+  `rowid` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(30) NOT NULL,
+  `intitule` varchar(80) NOT NULL,
+  `sort` smallint(6) NOT NULL,
+  `active` int(11) DEFAULT NULL,
+  `tms` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`rowid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
