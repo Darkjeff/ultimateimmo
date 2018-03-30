32e59786183db0e00eb533de69f1467b2d36d6a6
Philippe GRAND <philippe.grand@atoo-net.com>
2018-03-30 22:08:55 +0200

    add immocost

--- htdocs/custom/immobilier/class/immocost.class.php
+++ htdocs/custom/immobilier/class/immocost.class.php
@@ -1,6 +1,6 @@
 <?php
 /* Copyright (C) 2017  Laurent Destailleur <eldy@users.sourceforge.net>
- * Copyright (C) ---Put here your own copyright and developer email---
+ * Copyright (C) 2018 Philippe GRAND 	<philippe.grand@atoo-net.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -313,7 +313,7 @@ class ImmoCost extends CommonObject
         $label.= '<br>';
         $label.= '<b>' . $langs->trans('Ref') . ':</b> ' . $this->ref;
 
-        $url = dol_buildpath('/immobilier/immocost_card.php',1).'?id='.$this->id;
+        $url = dol_buildpath('/immobilier/cost/immocost_card.php',1).'?id='.$this->id;
 
         if ($option != 'nolink')
         {
