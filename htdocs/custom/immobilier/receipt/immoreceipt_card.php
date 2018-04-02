d141b2ede7b617143bdc35a73360fe7a9de0e39f
Philippe GRAND <philippe.grand@atoo-net.com>
2018-04-02 12:12:48 +0200

    a lot of debug

--- htdocs/custom/immobilier/receipt/immoreceipt_card.php
+++ htdocs/custom/immobilier/receipt/immoreceipt_card.php
@@ -56,11 +56,15 @@ if (! $res) die("Include of main fails");
 
 include_once(DOL_DOCUMENT_ROOT.'/core/class/html.formcompany.class.php');
 include_once(DOL_DOCUMENT_ROOT.'/core/class/html.formfile.class.php');
+include_once(DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php');
+require_once DOL_DOCUMENT_ROOT . '/core/class/doleditor.class.php';
 dol_include_once('/immobilier/class/immoreceipt.class.php');
 dol_include_once('/immobilier/lib/immoreceipt.lib.php');
+dol_include_once('/immobilier/core/modules/immobilier/modules_immobilier.php');
+dol_include_once('/immobilier/class/immorent.class.php');
 
 // Load traductions files requiredby by page
-$langs->loadLangs(array("immobilier@immobilier","other"));
+$langs->loadLangs(array("immobilier@immobilier", "other", "compta", "bills"));
 
 // Get parameters
 $id			= GETPOST('id', 'int');
@@ -132,7 +136,245 @@ if (empty($reshook))
 	include DOL_DOCUMENT_ROOT.'/core/actions_sendmails.inc.php';
 }
 
+/*
+ * 	Classify paid
+ */
+if ($action == 'paid') {
+	$receipt = new ImmoReceipt($db);
+	$receipt->fetch($id);
+	$result = $receipt->set_paid($user);
+	Header("Location: " . $_SERVER['PHP_SELF'] . "?id=" . $id);
+}
 
+/*
+ *	Delete rental
+ */
+if ($action == 'confirm_delete' && $_REQUEST["confirm"] == 'yes') {
+	$receipt = new ImmoReceipt($db);
+	$receipt->fetch($id);
+	$result = $receipt->delete($user);
+	if ($result > 0) {
+		header("Location: ".$backurlforlist);
+		exit();
+	} else {
+		$mesg = '<div class="error">' . $receipt->error . '</div>';
+	}
+}
+
+/*
+ * Action generate quittance
+ */
+if ($action == 'quittance') {
+	// Define output language
+	$outputlangs = $langs;
+	
+	$file = 'quittance_' . $id . '.pdf';
+	
+	$result = immobilier_pdf_create($db, $id, '', 'quittance', $outputlangs, $file);
+	
+	if ($result > 0) {
+		Header("Location: " . $_SERVER['PHP_SELF'] . "?id=" . $id);
+		exit();
+	} else {
+		setEventMessage($agf->error, 'errors');
+	}
+}
+
+/*
+ * Action generate charge locative
+ */
+if ($action == 'chargeloc') {
+	// Define output language
+	$outputlangs = $langs;
+	
+	$file = 'chargeloc_' . $id . '.pdf';
+	
+	$result = immobilier_pdf_create($db, $id, '', 'chargeloc', $outputlangs, $file);
+	
+	if ($result > 0) {
+		Header("Location: " . $_SERVER['PHP_SELF'] . "?id=" . $id);
+		exit();
+	} else {
+		setEventMessage($agf->error, 'errors');
+	}
+}
+
+/*
+ * Add rental
+ */
+if ($action == 'add' && ! $cancel) {
+	$error = 0;
+	
+	$datev = dol_mktime(12, 0, 0, GETPOST("datevmonth"), GETPOST("datevday"), GETPOST("datevyear"));
+	$datesp = dol_mktime(12, 0, 0, GETPOST("datespmonth"), GETPOST("datespday"), GETPOST("datespyear"));
+	$dateep = dol_mktime(12, 0, 0, GETPOST("dateepmonth"), GETPOST("dateepday"), GETPOST("dateepyear"));
+	
+	$object->nom = GETPOST("nom");
+	$object->datesp = $datesp;
+	$object->dateep = $dateep;
+	$object->datev = $datev;
+	
+	if (empty($datev) || empty($datesp) || empty($dateep)) {
+		setEventMessage($langs->trans("ErrorFieldRequired", $langs->transnoentitiesnoconv("Date")), 'errors');
+		$error ++;
+	}
+	
+	if (! $error) {
+		$db->begin();
+		
+		$ret = $object->create($user);
+		if ($ret > 0) {
+			$db->commit();
+			header("Location: index.php");
+			exit();
+		} else {
+			$db->rollback();
+			setEventMessages($object->error, $object->errors, 'errors');
+			$action = "create";
+		}
+	}
+	
+	$action = 'create';
+}
+
+/*
+ * Add all rental
+ */
+
+if ($action == 'addall') 
+{	
+	$error=0;
+	$dateech = dol_mktime(12,0,0, GETPOST("echmonth"), GETPOST("echday"), GETPOST("echyear"));
+	$dateperiod = dol_mktime(12,0,0, GETPOST("periodmonth"), GETPOST("periodday"), GETPOST("periodyear"));
+	$dateperiodend = dol_mktime(12,0,0, GETPOST("periodendmonth"), GETPOST("periodendday"), GETPOST("periodendyear"));
+	
+	if (empty($dateech)) {
+		setEventMessage($langs->trans("ErrorFieldRequired", $langs->transnoentities("DateDue")), 'errors');
+		$action = 'create';
+	} elseif (empty($dateperiod)) {
+		$mesg = '<div class="error">' . $langs->trans("ErrorFieldRequired", $langs->transnoentities("Period")) . '</div>';
+		$action = 'create';
+	} elseif (empty($dateperiodend)) {
+		$mesg = '<div class="error">' . $langs->trans("ErrorFieldRequired", $langs->transnoentities("Periodend")) . '</div>';
+		$action = 'create';
+	} 
+	else 
+	{
+		if(is_array($mesLignesCochees) && count($mesLignesCochees)>0)
+        {
+			$mesLignesCochees = GETPOST('mesCasesCochees');
+		
+			foreach ( $mesLignesCochees as $maLigneCochee ) 
+			{
+				
+				$receipt = new ImmoReceipt($db);
+				
+				$maLigneCourante = split("_", $maLigneCochee);
+				$monId = $maLigneCourante[0];
+				$monLocal = $maLigneCourante[1];
+				$monLocataire = $maLigneCourante[2];
+				$monMontant = $maLigneCourante[3];
+				$monLoyer = $maLigneCourante[4];
+				$monCharges = $maLigneCourante[5];
+				$monTVA = $maLigneCourante[7];
+				$monOwner = $maLigneCourante[6];
+				
+				// main info loyer
+				$receipt->name = GETPOST('name', 'alpha');
+				$receipt->echeance = $dateech;
+				$receipt->date_start = $dateperiod;
+				$receipt->date_end = $dateperiodend;
+				
+				// main info contrat
+				$receipt->fk_contract = $monId;
+				$receipt->fk_property = $monLocal;
+				$receipt->fk_renter = $monLocataire;
+				$receipt->fk_owner = $monOwner;
+				If ($monTVA == Oui) {
+				$receipt->amount_total = $monMontant * 1.2;
+				$receipt->vat = $monMontant * 0.2;}
+				Else {
+				$receipt->amount_total = $monMontant;}
+				
+				$receipt->rent = $monLoyer;
+				$receipt->charges = $monCharges;
+				$receipt->statut=0;
+				$receipt->paye=0;
+				
+				$result = $receipt->create($user);
+				if ($result < 0) {
+					$error++;
+					setEventMessages(null,$receipt->errors, 'errors');
+					$action='createall';
+				}
+			}
+		}
+	}
+	
+	if (empty($error)) {
+		setEventMessage($langs->trans("SocialContributionAdded"), 'mesgs');
+		Header("Location: " .$backurlforlist);
+		exit();
+	}
+}
+
+/*
+ * Edit Receipt
+ */
+
+if ($action == 'update')
+{
+	$dateech = @dol_mktime(12,0,0, GETPOST("echmonth"), GETPOST("echday"), GETPOST("echyear"));
+	$dateperiod = @dol_mktime(12,0,0, GETPOST("periodmonth"), GETPOST("periodday"), GETPOST("periodyear"));
+	$dateperiodend = @dol_mktime(12,0,0, GETPOST("periodendmonth"), GETPOST("periodendday"), GETPOST("periodendyear"));
+	/*if (! $dateech)
+	 {
+	 $mesg='<div class="error">'.$langs->trans("ErrorFieldRequired",$langs->transnoentities("DateDue")).'</div>';
+	 $action = 'update';
+	 }
+	 elseif (! $dateperiod)
+	 {
+	 $mesg='<div class="error">'.$langs->trans("ErrorFieldRequired",$langs->transnoentities("Period")).'</div>';
+	 $action = 'update';
+	 }
+	 elseif (! $dateperiodend)
+	 {
+	 $mesg='<div class="error">'.$langs->trans("ErrorFieldRequired",$langs->transnoentities("Periodend")).'</div>';
+	 $action = 'update';
+	 }
+	 else
+	 {
+	 */
+	$receipt = new ImmoReceipt($db);
+	$result = $receipt->fetch($id);
+	
+	$receipt->label 			= GETPOST('label');
+	If ($receipt->addtva != 0) {
+	$receipt->total_amount 	= ($_POST["rentamount"] + $_POST["charges"])*1.2;}
+	Else {
+	$receipt->total_amount 	= $_POST["rentamount"] + $_POST["charges"];}
+	$receipt->rentamount 			= $_POST["rentamount"];
+	$receipt->charges 		= $_POST["charges"];
+	If ($receipt->addtva != 0) {
+	$receipt->vat 			= ($_POST["rentamount"]+$_POST["charges"])*0.2;}
+	Else {
+	$receipt->vat 			= 0;}
+	
+	$receipt->echeance 		= $dateech;
+	$receipt->note_public 	= $_POST["note_public"];
+	$receipt->status 		= $_POST["status"];
+	$receipt->date_start 	= $dateperiod;
+	$receipt->date_end 		= $dateperiodend;
+	
+	$result = $receipt->update($user);
+	header("Location: " .$backurlforlist.'?id='.$receipt->id);
+	if ($id > 0) {
+		// $mesg='<div class="ok">'.$langs->trans("SocialContributionAdded").'</div>';
+	} else 
+	{
+		$mesg = '<div class="error">' . $receipt->error . '</div>';
+	}
+}
 
 
 /*
@@ -165,7 +407,26 @@ jQuery(document).ready(function() {
 // Part to create
 if ($action == 'create')
 {
-	print load_fiche_titre($langs->trans("NewObject", $langs->transnoentitiesnoconv("ImmoReceipt")));
+	llxheader('', $langs->trans("Addnewrent"), '');
+	
+	$year_current = strftime("%Y", dol_now());
+	$pastmonth = strftime("%m", dol_now());
+	$pastmonthyear = $year_current;
+	if ($pastmonth == 0) {
+		$pastmonth = 12;
+		$pastmonthyear --;
+	}
+	
+	$datesp = dol_mktime(0, 0, 0, $datespmonth, $datespday, $datespyear);
+	$dateep = dol_mktime(23, 59, 59, $dateepmonth, $dateepday, $dateepyear);
+	
+	if (empty($datesp) || empty($dateep)) // We define date_start and date_end
+	{
+		$datesp = dol_get_first_day($pastmonthyear, $pastmonth, false);
+		$dateep = dol_get_last_day($pastmonthyear, $pastmonth, false);
+	}
+	
+	print load_fiche_titre($langs->trans("NewReceipt", $langs->transnoentitiesnoconv("ImmoReceipt")));
 
 	print '<form method="POST" action="'.$_SERVER["PHP_SELF"].'">';
 	print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
@@ -192,6 +453,131 @@ if ($action == 'create')
 	print '<input type="'.($backtopage?"submit":"button").'" class="button" name="cancel" value="'.dol_escape_htmltag($langs->trans("Cancel")).'"'.($backtopage?'':' onclick="javascript:history.go(-1)"').'>';	// Cancel for create does not post form if we don't know the backtopage
 	print '</div>';
 
+	print '</form>';
+}
+/* *************************************************************************** */
+/*                                                                             */
+/* Mode add all contract                                                       */
+/*                                                                             */
+/* *************************************************************************** */
+
+elseif ($action == 'createall') 
+{
+	llxheader('', $langs->trans("newrental"), '');
+	print '<form name="fiche_loyer" method="post" action="' . $_SERVER["PHP_SELF"] . '">';
+	print '<input type="hidden" name="token" value="' . $_SESSION['newtoken'] . '">';
+	print '<input type="hidden" name="action" value="addall">';
+	print '<input type="hidden" name="backtopage" value="'.$backtopage.'">';
+	
+	print '<table class="border" width="100%">';
+	
+	print "<tr class=\"liste_titre\">";
+	
+	print '<td align="left">';
+	print $langs->trans("NomLoyer");
+	print '</td><td align="center">';
+	print $langs->trans("Echeance");
+	print '</td><td align="center">';
+	print $langs->trans("Periode_du");
+	print '</td><td align="center">';
+	print $langs->trans("Periode_au");
+	print '</td><td align="left">';
+	print '&nbsp;';
+	print '</td>';
+	print "</tr>\n";
+	
+	print '<tr class="oddeven" valign="top">';
+	
+	/*
+	 * Nom du loyer
+	 */
+	print '<td><input name="name" size="30" value="' . GETPOST('name') . '"</td>';
+	
+	// Due date
+	print '<td align="center">';
+	print $form->select_date(! empty($dateech) ? $dateech : '-1', 'ech', 0, 0, 0, 'fiche_loyer', 1);
+	print '</td>';
+	print '<td align="center">';
+	print $form->select_date(! empty($dateperiod) ? $dateperiod : '-1', 'period', 0, 0, 0, 'fiche_loyer', 1);
+	print '</td>';
+	print '<td align="center">';
+	print $form->select_date(! empty($dateperiodend) ? $dateperiodend : '-1', 'periodend', 0, 0, 0, 'fiche_loyer', 1);
+	print '</td>';
+	
+	print '<td align="center"><input type="submit" class="button" value="' . $langs->trans("Add") . '"></td></tr>';
+	
+	print '</table>';
+	
+	/*
+	 * List agreement
+	 */
+	$sql = "SELECT c.rowid as reference, loc.label as nom, l.address, l.label as local, loc.status as status, c.montant_tot as total,";
+	$sql .= "c.loyer , c.charges, c.fk_renter as reflocataire, c.fk_property as reflocal, c.preavis as preavis, c.tva, l.fk_owner";
+	$sql .= " FROM " . MAIN_DB_PREFIX . "immobilier_immorenter loc";
+	$sql .= " , " . MAIN_DB_PREFIX . "immobilier_immocontrat as c";
+	$sql .= " , " . MAIN_DB_PREFIX . "immobilier_immoproperty as l";
+	$sql .= " WHERE preavis = 0 AND loc.rowid = c.fk_renter and l.rowid = c.fk_property  ";
+	$resql = $db->query($sql);
+	if ($resql) 
+	{
+		$num = $db->num_rows($resql);
+		
+		$i = 0;
+		$total = 0;
+		
+		print '<br><table class="noborder" width="100%">';
+		print '<tr class="liste_titre">';
+		print '<td>' . $langs->trans('Contract') . '</td>';
+		print '<td>' . $langs->trans('Property') . '</td>';
+		print '<td>' . $langs->trans('Nomlocal') . '</td>';
+		print '<td>' . $langs->trans('Renter') . '</td>';
+		print '<td>' . $langs->trans('NameRenter') . '</td>';
+		print '<td align="right">' . $langs->trans('AmountTC') . '</td>';
+		print '<td align="right">' . $langs->trans('Rent') . '</td>';
+		print '<td align="right">' . $langs->trans('Charges') . '</td>';
+		print '<td align="right">' . $langs->trans('VATIsUsed') . '</td>';
+		print '<td align="right">' . $langs->trans('nameowner') . '</td>';
+		print '<td align="right">' . $langs->trans('Select') . '</td>';
+		print "</tr>\n";
+		
+		if ($num > 0) 
+		{
+			
+			while ( $i < $num ) 
+			{
+				$objp = $db->fetch_object($resql);
+				print '<tr class="oddeven">';				
+				print '<td>' . $objp->reference . '</td>';
+				print '<td>' . $objp->reflocal . '</td>';
+				print '<td>' . $objp->local . '</td>';
+				print '<td>' . $objp->reflocataire . '</td>';
+				print '<td>' . $objp->nom . '</td>';
+				
+				print '<td align="right">' . price($objp->total) . '</td>';
+				print '<td align="right">' . price($objp->loyer) . '</td>';
+				print '<td align="right">' . price($objp->charges) . '</td>';
+				print '<td align="right">' . yn($objp->tva) . '</td>';
+				print '<td align="right">' . $objp->fk_owner . '</td>';
+				
+				// Colonne choix contrat
+				print '<td align="center">';
+				
+				print '<input type="checkbox" name="mesCasesCochees[]" value="' . $objp->reference . '_' . $objp->reflocal . '_' . $objp->reflocataire . '_' . $objp->total . '_' . $objp->loyer . '_' . $objp->charges . '_' . $objp->fk_owner . '"' . ($objp->reflocal ? ' checked="checked"' : "") . '/>';
+				print '</td>';
+				print '</tr>';
+				
+				$i ++;
+			}
+		}
+		
+		print "</table>\n";
+		$db->free($resql);
+	} 
+
+	else 
+	{
+		dol_print_error($db);
+	}
 	print '</form>';
 }
 
