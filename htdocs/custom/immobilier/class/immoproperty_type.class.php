e502a15582b32ae1b152ed814de3d8bd7da83871
Philippe GRAND <philippe.grand@atoo-net.com>
2018-10-25 19:01:06 +0200

    translations

--- htdocs/custom/immobilier/class/immoproperty_type.class.php
+++ htdocs/custom/immobilier/class/immoproperty_type.class.php
@@ -86,7 +86,7 @@ class ImmoProperty_Type extends CommonObject
 		'tms' => array('type'=>'timestamp', 'label'=>'DateModification', 'enabled'=>1, 'visible'=>-2, 'position'=>501, 'notnull'=>1,),
 		'fk_user_creat' => array('type'=>'integer', 'label'=>'UserAuthor', 'enabled'=>1, 'visible'=>-2, 'position'=>510, 'notnull'=>1, 'foreignkey'=>'llx_user.rowid',),
 		'fk_user_modif' => array('type'=>'integer', 'label'=>'UserModif', 'enabled'=>1, 'visible'=>-2, 'position'=>511, 'notnull'=>-1,),
-		'status' => array('type'=>'integer', 'label'=>'Status', 'enabled'=>1, 'visible'=>1, 'position'=>1000, 'notnull'=>1, 'index'=>1, 'arrayofkeyval'=>array('0'=>'PropertyTypeStatusDraft', '1'=>'PropertyTypeStatusActive', '-1'=>'PropertyTypeStatusCancel')),
+		'status' => array('type'=>'integer', 'label'=>'Status', 'enabled'=>1, 'visible'=>1, 'position'=>1000, 'notnull'=>1, 'index'=>1, 'arrayofkeyval'=>array('0'=>'Draft', '1'=>'Active', '-1'=>'Cancel')),
 	);
 	public $rowid;
 	public $ref;
