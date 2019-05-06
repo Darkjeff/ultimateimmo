d0942ead0dd9deee733b5091922eefa0aa040640
Philippe GRAND <philippe.grand@atoo-net.com>
2019-05-06 11:30:58 +0200

    add data for dictionnary

--- sql/data.sql
+++ sql/data.sql
@@ -19,6 +19,10 @@ INSERT INTO llx_c_ultimateimmo_immoreceipt_status(rowid,code,label,active) VALUE
 
 INSERT INTO llx_c_ultimateimmo_immoreceipt_status(rowid,code,label,active) VALUES (1,'STATUS_VALIDATED','Validée',1);
 
+INSERT INTO llx_c_ultimateimmo_immorent_type(rowid,code,label,active) VALUES (0,'EMPTY_HOUSING','Logement vide',1);
+
+INSERT INTO llx_c_ultimateimmo_immorent_type(rowid,code,label,active) VALUES (1,'FURNISHED_APARTMENT','Logement meublé',1);
+
 
 
 
