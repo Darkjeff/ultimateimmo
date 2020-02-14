ebc63a0155534a404587d89744ea36ff95520739
Philippe GRAND <philippe.grand@atoo-net.com>
2020-02-14 15:47:02 +0100

    update and clean code

--- property/immoproperty_card.php
+++ property/immoproperty_card.php
@@ -208,13 +208,13 @@ if ($action == 'create')
 
 	// Common attributes
 	$object->fields = dol_sort_array($object->fields, 'position');
-
+	
 	foreach($object->fields as $key => $val)
 	{
 		// Discard if extrafield is a hidden field on form
-		if (abs($val['visible']) != 1 && abs($val['visible']) != 3) continue;
+		if (abs($val['visible']) != 1) continue;
 
-		if (array_key_exists('enabled', $val) && isset($val['enabled']) && ! verifCond($val['enabled'])) continue;	// We don't want this field
+		if (array_key_exists('enabled', $val) && isset($val['enabled']) && ! $val['enabled']) continue;	// We don't want this field
 
 		print '<tr id="field_'.$key.'">';
 		print '<td';
