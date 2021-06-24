ac28526ac7c2cc6d00dae102bab6f545d4c24167
Philippe GRAND <philippe.grand@atoo-net.com>
2021-06-24 17:03:19 +0200

    Add page bilan

--- renter/immorenter_bilan.php
+++ renter/immorenter_bilan.php
@@ -1,7 +1,7 @@
 ﻿<?php
 /* Copyright (C) 2013-2016 Olivier Geffroy		<jeff@jeffinfo.com>
  * Copyright (C) 2015-2016 Alexandre Spangaro	<aspangaro@zendsi.com>
- * Copyright (C) 2018-2019 Philippe GRAND 	    <philippe.grand@atoo-net.com>
+ * Copyright (C) 2018-2021 Philippe GRAND 	    <philippe.grand@atoo-net.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,16 +22,31 @@
  * \ingroup 	ultimateimmo
  * \brief 		Page fiche locataire
  */
-$res = @include ("../../main.inc.php"); // For root directory
-if (! $res)
-	$res = @include ("../../../main.inc.php"); // For "custom" directory
-if (! $res)
-	die("Include of main fails");
-	
+// Load Dolibarr environment
+$res = 0;
+// Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
+if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res = @include($_SERVER["CONTEXT_DOCUMENT_ROOT"] . "/main.inc.php");
+// Try main.inc.php into web root detected using web root caluclated from SCRIPT_FILENAME
+$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME'];
+$tmp2 = realpath(__FILE__);
+$i = strlen($tmp) - 1;
+$j = strlen($tmp2) - 1;
+while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
+	$i--;
+	$j--;
+}
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1)) . "/main.inc.php")) $res = @include(substr($tmp, 0, ($i + 1)) . "/main.inc.php");
+if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1))) . "/main.inc.php")) $res = @include(dirname(substr($tmp, 0, ($i + 1))) . "/main.inc.php");
+// Try main.inc.php using relative path
+if (!$res && file_exists("../main.inc.php")) $res = @include("../main.inc.php");
+if (!$res && file_exists("../../main.inc.php")) $res = @include("../../main.inc.php");
+if (!$res && file_exists("../../../main.inc.php")) $res = @include("../../../main.inc.php");
+if (!$res) die("Include of main fails");
+
 // Class
-dol_include_once ( "/ultimateimmo/class/immorent.class.php" );
-require_once ('../class/immorenter.class.php');
-require_once ('../core/lib/ultimateimmo.lib.php');
+dol_include_once("/ultimateimmo/class/immorent.class.php");
+dol_include_once('/ultimateimmo/class/immorenter.class.php');
+dol_include_once('/ultimateimmo/lib/immorenter.lib.php');
 
 // Langs
 $langs->load ( "ultimateimmo@ultimateimmo" );
@@ -53,18 +68,28 @@ llxheader ( '', $langs->trans("Renter").' | '.$langs->trans("Bilan"), '' );
 
 $object->fetch_thirdparty();
 
-$head=renter_prepare_head($object);
+if (!empty($conf->notification->enabled)) $langs->load("mails");
+$head = immorenterPrepareHead($object);
 
 dol_fiche_head($head, 'bilan',  $langs->trans("Renter"), 0, 'user');
 
-$linkback = '<a href="./list.php'.(! empty($socid)?'?socid='.$socid:'').'">'.$langs->trans("BackToList").'</a>';
+// Object card
+// ------------------------------------------------------------
+$linkback = '<a href="' . dol_buildpath('/ultimateimmo/renter/immorenter_list.php', 1) . '?restore_lastsearch_values=1' . (!empty($socid) ? '&socid=' . $socid : '') . '">' . $langs->trans("BackToList") . '</a>';
 
-immo_banner_tab($object, 'id', $linkback, 1, 'rowid', 'name');
+$morehtmlref = '<div class="refidno">';
 
-print '<table class="border centpercent">';
+// Thirdparty
+$morehtmlref .= '<br>' . $langs->trans('ThirdParty') . ' : ' . (is_object($object->thirdparty) ? $object->thirdparty->getNomUrl(1) : '');
+if (empty($conf->global->MAIN_DISABLE_OTHER_LINK) && $object->thirdparty->id > 0) $morehtmlref .= ' (<a href="' . dol_buildpath('/ultimateimmo/rent/immorent_list.php', 1) . '?socid=' . $object->thirdparty->id . '&search_fk_soc=' . urlencode($object->thirdparty->id) . '">' . $langs->trans("OtherRents") . '</a>)';
 
+$morehtmlref .= '</div>';
+
+dol_banner_tab($object, 'ref', $linkback, 1, 'ref', 'ref', $morehtmlref);
+
+print '<div class="fichecenter">';
 print '<div class="underbanner clearboth"></div>';
-	
+
 $sql = "(SELECT l.date_start as date , l.total_amount as debit, 0 as credit , l.label as des";
 $sql .= " FROM " . MAIN_DB_PREFIX . "ultimateimmo_immoreceipt as l";
 $sql .= " WHERE l.fk_renter =" . $id;
@@ -75,17 +100,17 @@ $sql .= " WHERE p.fk_renter =" . $id;
 $sql .= ")";
 $sql .= "ORDER BY date";
 
-$result = $db->query ( $sql );
+$result = $db->query($sql);
 if ($result) {
-	$num_lignes = $db->num_rows ( $result );
+	$num_lignes = $db->num_rows($result);
 	$i = 0;
 
 	print '<table class="border tableforfield" width="100%">';
 	print '<tr class="liste_titre">';
-	print '<td>' . $langs->trans ( "Date" ) . '</td>';
-	print '<td>' . $langs->trans ( "Debit" ) . '</td>';
-	print '<td>' . $langs->trans ( "Credit" ) . '</td>';
-	print '<td>' . $langs->trans ( "Description" ) . '</td>';
+	print '<td>' . $langs->trans("Date") . '</td>';
+	print '<td>' . $langs->trans("Debit") . '</td>';
+	print '<td>' . $langs->trans("Credit") . '</td>';
+	print '<td>' . $langs->trans("Description") . '</td>';
 	print "</tr>\n";
 
 	$sql2 = "SELECT SUM(l.total_amount) as debit, 0 as credit ";
@@ -96,39 +121,33 @@ if ($result) {
 	$sql3 .= " FROM " . MAIN_DB_PREFIX . "ultimateimmo_immopayment as p";
 	$sql3 .= " WHERE p.fk_renter =" . $id;
 
-	$result2 = $db->query ( $sql2 );
-	$result3 = $db->query ( $sql3 );
+	$result2 = $db->query($sql2);
+	$result3 = $db->query($sql3);
 
-	$objp2 = $db->fetch_object ( $result2 );
-	$objp3 = $db->fetch_object ( $result3 );
+	$objp2 = $db->fetch_object($result2);
+	$objp3 = $db->fetch_object($result3);
 
-	$var = ! $var;
-
-	$var = True;
-	while ( $i < min ( $num_lignes, $limit ) )
-	{
-		$objp = $db->fetch_object ( $result );
-		$var = ! $var;
-		print "<tr $bc[$var]>";
-
-		print '<td>' . dol_print_date ( $db->jdate ( $objp->date ), 'day' ) . '</td>';
-		print '<td align="right">' . price($objp->debit) . '</td>';
-		print '<td align="right">' . price($objp->credit) . '</td>';
+	while ($i < min($num_lignes, $limit)) {
+		$objp = $db->fetch_object($result);
+		print '<tr class="oddeven">';
+		print '<td>' . dol_print_date($db->jdate($objp->date), 'day') . '</td>';
+		print '<td align="left">' . price($objp->debit) . '</td>';
+		print '<td align="left">' . price($objp->credit) . '</td>';
 		print '<td>' . $objp->des . '</td>';
 
 		print "</tr>";
-		$i ++;
+		$i++;
 	}
 
 	// Total
-	print "<tr $bc[$var]>";
+	print '<tr class="oddeven">';
 	print '<td>' . $langs->trans("Total") . '</td>';
-	print '<td align="right">' . price($objp2->debit) . '</td>';
-	print '<td align="right">' . price($objp3->credit) . '</td>';
-	print '<td>' . price(($objp3->credit)-($objp2->debit)). '</td>';
+	print '<td align="left">' . price($objp2->debit) . '</td>';
+	print '<td align="left">' . price($objp3->credit) . '</td>';
+	print '<td>' . price(($objp3->credit) - ($objp2->debit)) . '</td>';
 	print "</tr>";
 } else {
-	print $db->error ();
+	print $db->error();
 }
 
 print '</table>';
