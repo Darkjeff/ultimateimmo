f948fdddb3ddffcccf0c32aee33de3b5bf20fc7b
Philippe GRAND <philippe.grand@atoo-net.com>
2021-11-07 16:51:50 +0100

    add markdown for nice about page

--- admin/about.php
+++ admin/about.php
@@ -1,6 +1,6 @@
 <?php
 /* Copyright (C) 2004-2017 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2018 Philippe GRAND <philippe.grand@atoo-net.com>
+ * Copyright (C) 2018-2021 Philippe GRAND <philippe.grand@atoo-net.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
