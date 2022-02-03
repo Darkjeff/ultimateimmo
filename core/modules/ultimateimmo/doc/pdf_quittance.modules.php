92c43ed27d536e0dec3d0f9ffb3aee56ab384cfe
Florian HENRY <florian.henry@open-concept.pro>
2022-02-03 11:20:04 +0100

    fix pdf

--- core/modules/ultimateimmo/doc/pdf_quittance.modules.php
+++ core/modules/ultimateimmo/doc/pdf_quittance.modules.php
@@ -65,7 +65,7 @@ class pdf_quittance extends ModelePDFUltimateimmo
      * @var array() Minimum version of PHP required by module.
 	 * e.g.: PHP ≥ 5.6 = array(5, 6)
      */
-	public $phpmin = array(5, 6); 
+	public $phpmin = array(5, 6);
 
 	/**
      * Dolibarr version of the loaded document
@@ -77,32 +77,32 @@ class pdf_quittance extends ModelePDFUltimateimmo
      * @var int page_largeur
      */
     public $page_largeur;
-	
+
 	/**
      * @var int page_hauteur
      */
     public $page_hauteur;
-	
+
 	/**
      * @var array format
      */
     public $format;
-	
+
 	/**
      * @var int marge_gauche
      */
 	public $marge_gauche;
-	
+
 	/**
      * @var int marge_droite
      */
 	public $marge_droite;
-	
+
 	/**
      * @var int marge_haute
      */
 	public $marge_haute;
-	
+
 	/**
      * @var int marge_basse
      */
@@ -509,17 +509,17 @@ class pdf_quittance extends ModelePDFUltimateimmo
 						$i = 0;
 						$total = 0;
 
-						$text .= "<td align=\"right\">" . price($objp->total, 0, $outputlangs, 1, -1, -1, $conf->currency) . "</td>";
-
-						$this->db->free($resql);
+						$text .= "<tr><td align=\"right\">" . price($objp->total, 0, $outputlangs, 1, -1, -1, $conf->currency) . "</td></tr>";
 
 						$text .= "</table>";
 
 						$posY = $pdf->getY();
 
 						$pdf->SetFont(pdf_getPDFFont($outputlangs), '', 13);
-						$pdf->writeHTMLCell($widthbox, 0, $posX, $posY, dol_htmlentitiesbr($text), 1);
+						$pdf->writeHTMLCell($widthbox, 0, $posX, $posY, dol_htmlentitiesbr($text), 1, 0);
 					}
+
+					$this->db->free($resql);
 				}
 
 				// Bloc total Charge
@@ -596,8 +596,10 @@ class pdf_quittance extends ModelePDFUltimateimmo
 						$pdf->writeHTMLCell($widthbox, 0, $posX, $posY, dol_htmlentitiesbr($text), 1);
 					}
 				}*/
+
+
 			}
-			$this->db->free($resql);
+
 
 			$pdf->Close();
 
