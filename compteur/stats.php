d7f7422287e33bbd1c4ea545f0c1afa0038719cd
Florian HENRY <florian.henry@open-concept.pro>
2022-03-31 12:29:09 +0200

    fix stats

---
+++ compteur/stats.php
@@ -0,0 +1,179 @@
+<?php
+/* Copyright (C) 2001-2006 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ * Copyright (C) 2004-2005 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2013      Olivier Geffroy      <jeff@jeffinfo.com>
+ * Copyright (C) 2018-2019 Philippe GRAND 	    <philippe.grand@atoo-net.com>
+ * Copyright (C) 2020      Thomas OURSEL         <contact@ogest.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file ultimateimmo/compteur/stats.php
+ * \ingroup compta
+ * \brief Page accueil ventilation
+ */
+
+// Load Dolibarr environment
+$res = 0;
+// Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
+if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) {
+	$res = @include $_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php";
+}
+// Try main.inc.php into web root detected using web root calculated from SCRIPT_FILENAME
+$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME']; $tmp2 = realpath(__FILE__); $i = strlen($tmp) - 1; $j = strlen($tmp2) - 1;
+while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
+	$i--; $j--;
+}
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1))."/main.inc.php")) {
+	$res = @include substr($tmp, 0, ($i + 1))."/main.inc.php";
+}
+if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php")) {
+	$res = @include dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php";
+}
+// Try main.inc.php using relative path
+if (!$res && file_exists("../main.inc.php")) {
+	$res = @include "../main.inc.php";
+}
+if (!$res && file_exists("../../main.inc.php")) {
+	$res = @include "../../main.inc.php";
+}
+if (!$res && file_exists("../../../main.inc.php")) {
+	$res = @include "../../../main.inc.php";
+}
+if (!$res) {
+	die("Include of main fails");
+}
+// Class
+require_once DOL_DOCUMENT_ROOT . "/core/lib/date.lib.php";
+dol_include_once('/ultimateimmo/class/immocompteur.class.php');
+dol_include_once('/ultimateimmo/class/immoproperty.class.php');
+
+// Load traductions files requiredby by page
+$langs->loadLangs(array("ultimateimmo@ultimateimmo", "other", "bills"));
+
+$object = new ImmoCompteur($db);
+$properties = new ImmoProperty($db);
+
+/*
+ * View
+ */
+llxHeader('', $langs->trans('MenuImmoCompteurList') . ' ' .$langs->trans('MenuImmoCompteurStats'));
+
+
+print '<div class="div-table-responsive">'; // You can use div-table-responsive-no-min if you dont need reserved height for your table
+print '<table class="tagtable nobottomiftotal liste">'."\n";
+
+
+$sql = 'SELECT ';
+$sql .= $object->getFieldList('t');
+$sql .= " FROM ".MAIN_DB_PREFIX.$object->table_element." as t";
+$sql .= $db->order('t.fk_immoproperty,date_relever');
+//$sql .= " INNER JOIN " . MAIN_DB_PREFIX . "ultimateimmo_immoproperty as ip ON t.fk_immoproperty = ip.rowid";
+$result_data=array();
+
+$resql = $db->query($sql);
+if ($resql) {
+	$i = 0;
+	$num = $db->num_rows($resql);
+
+	while ($obj = $db->fetch_object($resql)) {
+		$result_data[$i] = $obj;
+		$i++;
+	}
+	$db->free($resql);
+} else {
+	print $db->lasterror(); // affiche la derniere erreur sql
+}
+
+
+
+
+// Fields title
+// --------------------------------------------------------------------
+print '<tr class="liste_titre">';
+
+print '<td class="left">' . $langs->trans("Building") . '</td>';
+print '<td class="left">' . $langs->trans("Date") . '</td>';
+print '<td class="left">' . $langs->trans("Relever") . '</td>';
+print '<td class="left">' . $langs->trans("Nb Jour") . '</td>';
+print '<td class="left">' . $langs->trans("Consommation") . '</td>';
+print '<td class="left">' . $langs->trans("Consommation journaliere") . '</td>';
+print '<td class="left">' . $langs->trans("Consommation mensuel") . '</td>';
+print '<td class="left">' . $langs->trans("Consommation annuel") . '</td>';
+print '<td class="left">' . $langs->trans("Year") . '</td>';
+print '<tr class="oddeven">';
+$i=0;
+foreach ($result_data as $obj) {
+	$result = $properties->fetch($obj->fk_immoproperty);
+	if ($result < 0) {
+		setEventMessages($properties->error, $properties->errors, 'errors');
+	}
+	print '<td class="left">' . $properties->getNomUrl() . '</td>';
+	print '<td class="left">' . dol_print_date($obj->date_relever) . '</td>';
+	print '<td class="left">' . $obj->qty . '</td>';
+	print '<td class="left">';
+	if (array_key_exists($i+1, $result_data) && $result_data[$i+1]->fk_immoproperty==$obj->fk_immoproperty) {
+		$day_diff = num_between_day($db->jdate($obj->date_relever), $db->jdate($result_data[$i+1]->date_relever));
+		print $day_diff;
+	} else {
+		$day_diff=1;
+	}
+	print '</td>';
+
+	//Consommation
+	print '<td class="left">';
+	if (array_key_exists($i+1, $result_data) && $result_data[$i+1]->fk_immoproperty==$obj->fk_immoproperty) {
+		$rel_diff = $result_data[$i+1]->qty-$obj->qty;
+		print $rel_diff;
+	} else {
+		$rel_diff=0;
+	}
+	print '</td>';
+
+	//Consommation jour
+	print '<td class="left">';
+	$conso_jour=0;
+	if (empty($day_diff)) {
+		$day_diff = 1;
+	}
+	$conso_jour=$rel_diff/$day_diff;
+	print price($conso_jour);
+	print '</td>';
+
+	//Consommation mois
+	print '<td class="left">';
+	$conso_mois=$conso_jour*30;
+	print price($conso_mois);
+	print '</td>';
+
+	//Consommation annuel
+	print '<td class="left">';
+	$conso_year=$conso_jour*365;
+	print price($conso_year);
+	print '</td>';
+
+	//Année
+	print '<td class="left">';
+	print dol_print_date($obj->date_relever, '%Y');
+	print '</td>';
+
+	print '</tr>';
+	$i++;
+}
+print "</table>\n";
+
+llxFooter();
+
+$db->close();
--- core/modules/modUltimateimmo.class.php
+++ core/modules/modUltimateimmo.class.php
@@ -633,6 +633,26 @@ class modUltimateimmo extends DolibarrModules
 			'user'     => 2
 		);        // 0=Menu for internal users, 1=external users, 2=both
 		$this->menu[$r++] = array(
+			'fk_menu'  => 'fk_mainmenu=properties,fk_leftmenu=ultimateimmo_immocompteur',
+			// '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
+			'type'     => 'left',                            // This is a Left menu entry
+			'titre'    => 'MenuImmoCompteurStats',
+			'mainmenu' => 'properties',
+			'leftmenu' => 'ultimateimmo_immocompteur_stats',
+			'url'      => '/ultimateimmo/compteur/stats.php',
+			'langs'    => 'ultimateimmo@ultimateimmo',
+			// Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
+			'position' => 1100 + $r,
+			'enabled'  => '$conf->ultimateimmo->enabled',
+			// Define condition to show or hide menu entry. Use '$conf->ultimateimmo->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
+			'perms'    => '$user->rights->ultimateimmo->immocompteur->read',
+			// Use 'perms'=>'$user->rights->ultimateimmo->level1->level2' if you want your menu with a permission rules
+			'target'   => '',
+			'user'     => 2
+		);        // 0=Menu for internal users, 1=external users, 2=both
+
+
+		$this->menu[$r++] = array(
 			'fk_menu'  => '',
 			// '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
 			'type'     => 'top',                            // This is a Left menu entry
