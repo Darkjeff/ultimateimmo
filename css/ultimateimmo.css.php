96a49c6789dc5935c418f4ca165eee68af0ddeb6
Philippe GRAND <philippe.grand@atoo-net.com>
2022-04-01 14:26:29 +0200

    update code

--- css/ultimateimmo.css.php
+++ css/ultimateimmo.css.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2018-2019 Philippe GRAND <philippe.grand@atoo-net.com>
+/* Copyright (C) 2018-2022 Philippe GRAND <philippe.grand@atoo-net.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,30 +23,36 @@
 
 //if (! defined('NOREQUIREUSER')) define('NOREQUIREUSER','1');	// Not disabled because need to load personalized language
 //if (! defined('NOREQUIREDB'))   define('NOREQUIREDB','1');	// Not disabled. Language code is found on url.
-if (! defined('NOREQUIRESOC'))    define('NOREQUIRESOC','1');
+if (!defined('NOREQUIRESOC'))    define('NOREQUIRESOC', '1');
 //if (! defined('NOREQUIRETRAN')) define('NOREQUIRETRAN','1');	// Not disabled because need to do translations
-if (! defined('NOCSRFCHECK'))     define('NOCSRFCHECK',1);
-if (! defined('NOTOKENRENEWAL'))  define('NOTOKENRENEWAL',1);
-if (! defined('NOLOGIN'))         define('NOLOGIN',1);          // File must be accessed by logon page so without login
+if (!defined('NOCSRFCHECK'))     define('NOCSRFCHECK', 1);
+if (!defined('NOTOKENRENEWAL'))  define('NOTOKENRENEWAL', 1);
+if (!defined('NOLOGIN'))         define('NOLOGIN', 1);          // File must be accessed by logon page so without login
 //if (! defined('NOREQUIREMENU'))   define('NOREQUIREMENU',1);  // We need top menu content
-if (! defined('NOREQUIREHTML'))   define('NOREQUIREHTML',1);
-if (! defined('NOREQUIREAJAX'))   define('NOREQUIREAJAX','1');
+if (!defined('NOREQUIREHTML'))   define('NOREQUIREHTML', 1);
+if (!defined('NOREQUIREAJAX'))   define('NOREQUIREAJAX', '1');
 
 // Load Dolibarr environment
-$res=0;
+$res = 0;
 // Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
-if (! $res && ! empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res=@include($_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php");
+if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res = @include($_SERVER["CONTEXT_DOCUMENT_ROOT"] . "/main.inc.php");
 // Try main.inc.php into web root detected using web root caluclated from SCRIPT_FILENAME
-$tmp=empty($_SERVER['SCRIPT_FILENAME'])?'':$_SERVER['SCRIPT_FILENAME'];$tmp2=realpath(__FILE__); $i=strlen($tmp)-1; $j=strlen($tmp2)-1;
-while($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i]==$tmp2[$j]) { $i--; $j--; }
-if (! $res && $i > 0 && file_exists(substr($tmp, 0, ($i+1))."/main.inc.php")) $res=@include(substr($tmp, 0, ($i+1))."/main.inc.php");
-if (! $res && $i > 0 && file_exists(substr($tmp, 0, ($i+1))."/../main.inc.php")) $res=@include(substr($tmp, 0, ($i+1))."/../main.inc.php");
+$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME'];
+$tmp2 = realpath(__FILE__);
+$i = strlen($tmp) - 1;
+$j = strlen($tmp2) - 1;
+while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
+	$i--;
+	$j--;
+}
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1)) . "/main.inc.php")) $res = @include(substr($tmp, 0, ($i + 1)) . "/main.inc.php");
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1)) . "/../main.inc.php")) $res = @include(substr($tmp, 0, ($i + 1)) . "/../main.inc.php");
 // Try main.inc.php using relative path
-if (! $res && file_exists("../../main.inc.php")) $res=@include("../../main.inc.php");
-if (! $res && file_exists("../../../main.inc.php")) $res=@include("../../../main.inc.php");
-if (! $res) die("Include of main fails");
+if (!$res && file_exists("../../main.inc.php")) $res = @include("../../main.inc.php");
+if (!$res && file_exists("../../../main.inc.php")) $res = @include("../../../main.inc.php");
+if (!$res) die("Include of main fails");
 
-require_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
+require_once DOL_DOCUMENT_ROOT . '/core/lib/functions2.lib.php';
 
 session_cache_limiter(FALSE);
 
@@ -59,58 +65,60 @@ session_cache_limiter(FALSE);
 
 
 // Define css type
-header('Content-type: text/css');
+top_httphead('text/css');
 // Important: Following code is to cache this file to avoid page request by browser at each Dolibarr page access.
 // You can use CTRL+F5 to refresh your browser cache.
-if (empty($dolibarr_nocache)) header('Cache-Control: max-age=3600, public, must-revalidate');
+if (empty($dolibarr_nocache)) header('Cache-Control: max-age=10800, public, must-revalidate');
 else header('Cache-Control: no-cache');
 
 //$path='/cabinetmed';    // This value may be used in future for external module to overwrite theme
 //$theme='cabinetmed';
-$path='';    	// This value may be used in future for external module to overwrite theme
-$theme='eldy';	// Value of theme
-if (! empty($conf->global->MAIN_OVERWRITE_THEME_RES)) { $path='/'.$conf->global->MAIN_OVERWRITE_THEME_RES; $theme=$conf->global->MAIN_OVERWRITE_THEME_RES; }
+$path = '';    	// This value may be used in future for external module to overwrite theme
+$theme = 'eldy';	// Value of theme
+if (!empty($conf->global->MAIN_OVERWRITE_THEME_RES)) {
+	$path = '/' . $conf->global->MAIN_OVERWRITE_THEME_RES;
+	$theme = $conf->global->MAIN_OVERWRITE_THEME_RES;
+}
 
 // Define image path files
-$dol_hide_topmenu=$conf->dol_hide_topmenu;
-$dol_hide_leftmenu=$conf->dol_hide_leftmenu;
-$dol_optimize_smallscreen=$conf->dol_optimize_smallscreen;
-$dol_no_mouse_hover=$conf->dol_no_mouse_hover;
-$dol_use_jmobile=$conf->dol_use_jmobile;
+$dol_hide_topmenu = $conf->dol_hide_topmenu;
+$dol_hide_leftmenu = $conf->dol_hide_leftmenu;
+$dol_optimize_smallscreen = $conf->dol_optimize_smallscreen;
+$dol_no_mouse_hover = $conf->dol_no_mouse_hover;
+$dol_use_jmobile = $conf->dol_use_jmobile;
 ?>
 
-legend
-{
-    font-weight: normal;
+legend {
+	font-weight: normal;
 	color: #442288;
 }
 
 div.mainmenu.properties {
-	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immoproperty.png',1) ?>);
+	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immoproperty.png', 1) ?>);
 }
 
 div.mainmenu.immoowners {
-	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immoowner.png',1) ?>);
+	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immoowner.png', 1) ?>);
 }
 
 div.mainmenu.immorenters {
-	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immorenter.png',1) ?>);
+	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immorenter.png', 1) ?>);
 }
 
 div.mainmenu.immorents {
-	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immorent.png',1) ?>);
+	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immorent.png', 1) ?>);
 }
 
 div.mainmenu.immoreceipts {
-	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immoreceipt.png',1) ?>);
+	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immoreceipt.png', 1) ?>);
 }
 
 div.mainmenu.rentalloads {
-	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immocost.png',1) ?>);
+	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immocost.png', 1) ?>);
 }
 
 div.mainmenu.result {
-	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immoresult.png',1) ?>);
+	background-image: url(<?php echo dol_buildpath('/ultimateimmo/img/immoresult.png', 1) ?>);
 }
 
 .quatrevingtpercent, .inputsearch {
@@ -122,5 +130,3 @@ div.mainmenu.result {
 .myclasscss {
 	/* ... */
 }
-
-
