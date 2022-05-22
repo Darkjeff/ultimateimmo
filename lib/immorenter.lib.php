15ca15aaa82287d32b662dfcc1ed4b8e112898dd
Philippe GRAND <grandoc_doliweb.com@dolifarm1>
2022-05-22 14:47:21 +0200

    no recipt tab into renter file

--- lib/immorenter.lib.php
+++ lib/immorenter.lib.php
@@ -41,11 +41,6 @@ function immorenterPrepareHead($object)
 	$head[$h][2] = 'card';
 	$h++;
 
-	$head[$h][0] = dol_buildpath("/ultimateimmo/receipt/immoreceipt_card.php", 1).'?id='.$object->id;
-	$head[$h][1] = $langs->trans("ImmoReceipt");
-	$head[$h][2] = 'receipt';
-	$h++;
-
 	$head[$h][0] = dol_buildpath('/ultimateimmo/renter/immorenter_bilan.php', 1) . '?id=' . $object->id;
 	$head[$h][1] = $langs->trans("Bilan");
 	$head[$h][2] = 'bilan';
