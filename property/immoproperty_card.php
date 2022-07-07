f55d6798e9af7b9579e467f5ff8a6353b48fb726
Philippe GRAND <grandoc_doliweb.com@dolifarm1>
2022-07-07 09:43:56 +0200

    missing translation

--- property/immoproperty_card.php
+++ property/immoproperty_card.php
@@ -500,7 +500,7 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 	if ($action == 'clone') {
 		// Create an array for form
 		$formquestion = array();
-		$formconfirm = $form->formconfirm($_SERVER["PHP_SELF"] . '?id=' . $object->id, $langs->trans('ToClone'), $langs->trans('ConfirmCloneImmoProperty', $object->ref), 'confirm_clone', $formquestion, 'yes', 1);
+		$formconfirm = $form->formconfirm($_SERVER["PHP_SELF"] . '?id=' . $object->id, $langs->trans('CloneImmoProperty'), $langs->trans('ConfirmCloneImmoProperty', $object->ref), 'confirm_clone', $formquestion, 'yes', 1);
 	}
 
 	// Confirmation of action xxxx
