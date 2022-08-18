3572d7095bfba3cbfe71e721397c8c13df14ce9d
Philippe GRAND <grandoc_doliweb.com@dolifarm1>
2022-08-18 16:43:32 +0200

    use overwrighting of class commonobject and add wikihelp

--- admin/renter_emails.php
+++ admin/renter_emails.php
@@ -31,22 +31,28 @@
  */
 
 // Load Dolibarr environment
-$res=0;
+$res = 0;
 // Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
-if (! $res && ! empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res=@include($_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php");
+if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res = @include($_SERVER["CONTEXT_DOCUMENT_ROOT"] . "/main.inc.php");
 // Try main.inc.php into web root detected using web root caluclated from SCRIPT_FILENAME
-$tmp=empty($_SERVER['SCRIPT_FILENAME'])?'':$_SERVER['SCRIPT_FILENAME'];$tmp2=realpath(__FILE__); $i=strlen($tmp)-1; $j=strlen($tmp2)-1;
-while($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i]==$tmp2[$j]) { $i--; $j--; }
-if (! $res && $i > 0 && file_exists(substr($tmp, 0, ($i+1))."/main.inc.php")) $res=@include(substr($tmp, 0, ($i+1))."/main.inc.php");
-if (! $res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i+1)))."/main.inc.php")) $res=@include(dirname(substr($tmp, 0, ($i+1)))."/main.inc.php");
+$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME'];
+$tmp2 = realpath(__FILE__);
+$i = strlen($tmp) - 1;
+$j = strlen($tmp2) - 1;
+while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
+	$i--;
+	$j--;
+}
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1)) . "/main.inc.php")) $res = @include(substr($tmp, 0, ($i + 1)) . "/main.inc.php");
+if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1))) . "/main.inc.php")) $res = @include(dirname(substr($tmp, 0, ($i + 1))) . "/main.inc.php");
 // Try main.inc.php using relative path
-if (! $res && file_exists("../main.inc.php")) $res=@include("../main.inc.php");
-if (! $res && file_exists("../../main.inc.php")) $res=@include("../../main.inc.php");
-if (! $res && file_exists("../../../main.inc.php")) $res=@include("../../../main.inc.php");
-if (! $res) die("Include of main fails");
+if (!$res && file_exists("../main.inc.php")) $res = @include("../main.inc.php");
+if (!$res && file_exists("../../main.inc.php")) $res = @include("../../main.inc.php");
+if (!$res && file_exists("../../../main.inc.php")) $res = @include("../../../main.inc.php");
+if (!$res) die("Include of main fails");
 
-require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
-require_once ('../lib/ultimateimmo.lib.php');
+require_once DOL_DOCUMENT_ROOT . '/core/lib/admin.lib.php';
+require_once('../lib/ultimateimmo.lib.php');
 
 // Load translation files required by the page
 $langs->loadLangs(array("admin", "ultimateimmo@ultimateimmo"));
@@ -56,7 +62,7 @@ if (!$user->admin) {
 }
 
 
-$oldtypetonewone = array('texte'=>'text', 'chaine'=>'string'); // old type to new ones
+$oldtypetonewone = array('texte' => 'text', 'chaine' => 'string'); // old type to new ones
 
 $action = GETPOST('action', 'aZ09');
 
@@ -64,9 +70,9 @@ $error = 0;
 
 // Editing global variables not related to a specific theme
 $constantes = array(
-	'RENTER_REMINDER_EMAIL'=>array('type'=>'yesno', 'label'=>$langs->trans('RENTER_REMINDER_EMAIL', $langs->transnoentities("Module2300Name"))),
-	'RENTER_EMAIL_TEMPLATE_REMIND_EXPIRATION' 	=>'emailtemplate:immorenter',
-	'RENTER_MAIL_FROM' =>'string',
+	'RENTER_REMINDER_EMAIL' => array('type' => 'yesno', 'label' => $langs->trans('RENTER_REMINDER_EMAIL', $langs->transnoentities("Module2300Name"))),
+	'RENTER_EMAIL_TEMPLATE_REMIND_EXPIRATION' 	=> 'emailtemplate:immorenter',
+	'RENTER_MAIL_FROM' => 'string',
 );
 
 
@@ -81,9 +87,9 @@ if ($action == 'updateall') {
 
 	$res = 0;
 	foreach ($constantes as $constname => $value) {
-		$constvalue = (GETPOSTISSET('constvalue_'.$constname) ? GETPOST('constvalue_'.$constname, 'alphanohtml') : GETPOST('constvalue'));
-		$consttype = (GETPOSTISSET('consttype_'.$constname) ? GETPOST('consttype_'.$constname, 'alphanohtml') : GETPOST('consttype'));
-		$constnote = (GETPOSTISSET('constnote_'.$constname) ? GETPOST('constnote_'.$constname, 'restricthtml') : GETPOST('constnote'));
+		$constvalue = (GETPOSTISSET('constvalue_' . $constname) ? GETPOST('constvalue_' . $constname, 'alphanohtml') : GETPOST('constvalue'));
+		$consttype = (GETPOSTISSET('consttype_' . $constname) ? GETPOST('consttype_' . $constname, 'alphanohtml') : GETPOST('consttype'));
+		$constnote = (GETPOSTISSET('constnote_' . $constname) ? GETPOST('constnote_' . $constname, 'restricthtml') : GETPOST('constnote'));
 
 		$typetouse = empty($oldtypetonewone[$consttype]) ? $consttype : $oldtypetonewone[$consttype];
 		$constvalue = preg_replace('/:immorenter$/', '', $constvalue);
@@ -109,9 +115,9 @@ if ($action == 'update' || $action == 'add') {
 	$constlineid = GETPOST('rowid', 'int');
 	$constname = GETPOST('constname', 'alpha');
 
-	$constvalue = (GETPOSTISSET('constvalue_'.$constname) ? GETPOST('constvalue_'.$constname, 'alphanohtml') : GETPOST('constvalue'));
-	$consttype = (GETPOSTISSET('consttype_'.$constname) ? GETPOST('consttype_'.$constname, 'alphanohtml') : GETPOST('consttype'));
-	$constnote = (GETPOSTISSET('constnote_'.$constname) ? GETPOST('constnote_'.$constname, 'restricthtml') : GETPOST('constnote'));
+	$constvalue = (GETPOSTISSET('constvalue_' . $constname) ? GETPOST('constvalue_' . $constname, 'alphanohtml') : GETPOST('constvalue'));
+	$consttype = (GETPOSTISSET('consttype_' . $constname) ? GETPOST('consttype_' . $constname, 'alphanohtml') : GETPOST('consttype'));
+	$constnote = (GETPOSTISSET('constnote_' . $constname) ? GETPOST('constnote_' . $constname, 'restricthtml') : GETPOST('constnote'));
 
 	$typetouse = empty($oldtypetonewone[$consttype]) ? $consttype : $oldtypetonewone[$consttype];
 	$constvalue = preg_replace('/:immorenter$/', '', $constvalue);
@@ -136,12 +142,11 @@ if ($action == 'update' || $action == 'add') {
 
 $form = new Form($db);
 
-$help_url = 'EN:Module_UltimateImmo|FR:Module_UltimateImmo';
+$wikihelp = 'EN:Module_UltimateImmo|FR:Module_UltimateImmo';
+llxHeader('', $langs->trans("UltimateimmoSetup"), $wikihelp);
 
-llxHeader('', $langs->trans("UltimateimmoSetup"), $help_url);
 
-
-$linkback = '<a href="'.DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1">'.$langs->trans("BackToModuleList").'</a>';
+$linkback = '<a href="' . DOL_URL_ROOT . '/admin/modules.php?restore_lastsearch_values=1">' . $langs->trans("BackToModuleList") . '</a>';
 
 print load_fiche_titre($langs->trans("UltimateimmoSetup"), $linkback, 'title_setup');
 
@@ -150,18 +155,18 @@ $head = ultimateimmoAdminPrepareHead();
 print dol_get_fiche_head($head, 'emails', $langs->trans("ModuleUltimateimmoName"), -1, 'user');
 
 // TODO Use global form
-print '<form action="'.$_SERVER["PHP_SELF"].'" method="POST">';
-print '<input type="hidden" name="token" value="'.newToken().'">';
+print '<form action="' . $_SERVER["PHP_SELF"] . '" method="POST">';
+print '<input type="hidden" name="token" value="' . newToken() . '">';
 print '<input type="hidden" name="action" value="updateall">';
 
-$helptext = '*'.$langs->trans("FollowingConstantsWillBeSubstituted").'<br>';
+$helptext = '*' . $langs->trans("FollowingConstantsWillBeSubstituted") . '<br>';
 $helptext .= '__DOL_MAIN_URL_ROOT__, __ID__, __FIRSTNAME__, __LASTNAME__, __FULLNAME__, ';
 $helptext .= '__COMPANY__, __ADDRESS__, __ZIP__, __TOWN__, __COUNTRY__, __EMAIL__, __BIRTH__, __TYPE__, ';
 //$helptext.='__YEAR__, __MONTH__, __DAY__';	// Not supported
 
 form_constantes($constantes, 3, $helptext);
 
-print '<div class="center"><input type="submit" class="button" value="'.$langs->trans("Update").'" name="update"></div>';
+print '<div class="center"><input type="submit" class="button" value="' . $langs->trans("Update") . '" name="update"></div>';
 print '</form>';
 
 print dol_get_fiche_end();
--- admin/renter_extrafields.php
+++ admin/renter_extrafields.php
@@ -3,7 +3,7 @@
  * Copyright (C) 2003		Jean-Louis Bergamo		<jlb@j1b.org>
  * Copyright (C) 2004-2012	Laurent Destailleur		<eldy@users.sourceforge.net>
  * Copyright (C) 2012		Regis Houssin			<regis.houssin@capnetworks.com>
- * Copyright (C) 2018-2019 	Philippe GRAND 			<philippe.grand@atoo-net.com>
+ * Copyright (C) 2018-2022 	Philippe GRAND 			<philippe.grand@atoo-net.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,38 +26,44 @@
  */
 
 // Load Dolibarr environment
-$res=0;
+$res = 0;
 // Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
-if (! $res && ! empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res=@include($_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php");
+if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res = @include($_SERVER["CONTEXT_DOCUMENT_ROOT"] . "/main.inc.php");
 // Try main.inc.php into web root detected using web root caluclated from SCRIPT_FILENAME
-$tmp=empty($_SERVER['SCRIPT_FILENAME'])?'':$_SERVER['SCRIPT_FILENAME'];$tmp2=realpath(__FILE__); $i=strlen($tmp)-1; $j=strlen($tmp2)-1;
-while($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i]==$tmp2[$j]) { $i--; $j--; }
-if (! $res && $i > 0 && file_exists(substr($tmp, 0, ($i+1))."/main.inc.php")) $res=@include(substr($tmp, 0, ($i+1))."/main.inc.php");
-if (! $res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i+1)))."/main.inc.php")) $res=@include(dirname(substr($tmp, 0, ($i+1)))."/main.inc.php");
+$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME'];
+$tmp2 = realpath(__FILE__);
+$i = strlen($tmp) - 1;
+$j = strlen($tmp2) - 1;
+while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
+    $i--;
+    $j--;
+}
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1)) . "/main.inc.php")) $res = @include(substr($tmp, 0, ($i + 1)) . "/main.inc.php");
+if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1))) . "/main.inc.php")) $res = @include(dirname(substr($tmp, 0, ($i + 1))) . "/main.inc.php");
 // Try main.inc.php using relative path
-if (! $res && file_exists("../main.inc.php")) $res=@include("../main.inc.php");
-if (! $res && file_exists("../../main.inc.php")) $res=@include("../../main.inc.php");
-if (! $res && file_exists("../../../main.inc.php")) $res=@include("../../../main.inc.php");
-if (! $res) die("Include of main fails");
+if (!$res && file_exists("../main.inc.php")) $res = @include("../main.inc.php");
+if (!$res && file_exists("../../main.inc.php")) $res = @include("../../main.inc.php");
+if (!$res && file_exists("../../../main.inc.php")) $res = @include("../../../main.inc.php");
+if (!$res) die("Include of main fails");
 
 // Libraries
 dol_include_once('/ultimateimmo/lib/ultimateimmo.lib.php');
-require_once DOL_DOCUMENT_ROOT.'/core/class/extrafields.class.php';
+require_once DOL_DOCUMENT_ROOT . '/core/class/extrafields.class.php';
 
 // Load traductions files requiredby by page
-$langs->loadLangs(array("ultimateimmo@ultimateimmo","admin"));
+$langs->loadLangs(array("ultimateimmo@ultimateimmo", "admin"));
 
 $extrafields = new ExtraFields($db);
 $form = new Form($db);
 
 // List of supported format
-$tmptype2label=ExtraFields::$type2label;
-$type2label=array('');
-foreach ($tmptype2label as $key => $val) $type2label[$key]=$langs->transnoentitiesnoconv($val);
+$tmptype2label = ExtraFields::$type2label;
+$type2label = array('');
+foreach ($tmptype2label as $key => $val) $type2label[$key] = $langs->transnoentitiesnoconv($val);
 
-$action=GETPOST('action', 'alpha');
-$attrname=GETPOST('attrname', 'alpha');
-$elementtype='ultimateimmo_immorenter'; // Must be the $table_element of the class that manage extrafield
+$action = GETPOST('action', 'alpha');
+$attrname = GETPOST('attrname', 'alpha');
+$elementtype = 'ultimateimmo_immorenter'; // Must be the $table_element of the class that manage extrafield
 
 if (!$user->admin) accessforbidden();
 
@@ -66,7 +72,7 @@ if (!$user->admin) accessforbidden();
  * Actions
  */
 
-require DOL_DOCUMENT_ROOT.'/core/actions_extrafields.inc.php';
+require DOL_DOCUMENT_ROOT . '/core/actions_extrafields.inc.php';
 
 
 
@@ -76,26 +82,25 @@ require DOL_DOCUMENT_ROOT.'/core/actions_extrafields.inc.php';
 
 $textobject = $langs->transnoentitiesnoconv("Renters");
 
-$help_url='';
-llxHeader('', $langs->trans('UltimateimmoSetup'));
+$wikihelp = 'EN:Module_UltimateImmo|FR:Module_UltimateImmo';
+llxHeader('', $langs->trans('UltimateimmoSetup'), $wikihelp);
 
-$linkback='<a href="'.DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1">'.$langs->trans("BackToModuleList").'</a>';
-print load_fiche_titre($langs->trans("UltimateimmoSetup"),$linkback,'title_setup');
+$linkback = '<a href="' . DOL_URL_ROOT . '/admin/modules.php?restore_lastsearch_values=1">' . $langs->trans("BackToModuleList") . '</a>';
+print load_fiche_titre($langs->trans("UltimateimmoSetup"), $linkback, 'title_setup');
 
 $head = ultimateimmoAdminPrepareHead();
 
-dol_fiche_head($head, 'attributes_renter', $langs->trans("ModuleUltimateimmoName"), 0, 'building@ultimateimmo');
+print dol_get_fiche_head($head, 'attributes_renter', $langs->trans("ModuleUltimateimmoName"), 0, 'building@ultimateimmo');
 
-require DOL_DOCUMENT_ROOT.'/core/tpl/admin_extrafields_view.tpl.php';
+require DOL_DOCUMENT_ROOT . '/core/tpl/admin_extrafields_view.tpl.php';
 
-dol_fiche_end();
+print dol_get_fiche_end();
 
 
 // Buttons
-if ($action != 'create' && $action != 'edit')
-{
-	print '<div class="tabsAction">';
-    print "<a class=\"butAction\" href=\"".$_SERVER["PHP_SELF"]."?action=create\">".$langs->trans("NewAttribute")."</a>";
+if ($action != 'create' && $action != 'edit') {
+    print '<div class="tabsAction">';
+    print "<a class=\"butAction\" href=\"" . $_SERVER["PHP_SELF"] . "?action=create\">" . $langs->trans("NewAttribute") . "</a>";
     print "</div>";
 }
 
@@ -106,12 +111,11 @@ if ($action != 'create' && $action != 'edit')
 /*                                                                            */
 /* ************************************************************************** */
 
-if ($action == 'create')
-{
-	print '<div name="topofform"></div><br>';
+if ($action == 'create') {
+    print '<div name="topofform"></div><br>';
     print load_fiche_titre($langs->trans('NewAttribute'));
 
-    require DOL_DOCUMENT_ROOT.'/core/tpl/admin_extrafields_add.tpl.php';
+    require DOL_DOCUMENT_ROOT . '/core/tpl/admin_extrafields_add.tpl.php';
 }
 
 /* ************************************************************************** */
@@ -119,12 +123,11 @@ if ($action == 'create')
 /* Edition of an optional field                                               */
 /*                                                                            */
 /* ************************************************************************** */
-if ($action == 'edit' && ! empty($attrname))
-{
-	print '<div name="topofform"></div><br>';
+if ($action == 'edit' && !empty($attrname)) {
+    print '<div name="topofform"></div><br>';
     print load_fiche_titre($langs->trans("FieldEdition", $attrname));
 
-    require DOL_DOCUMENT_ROOT.'/core/tpl/admin_extrafields_edit.tpl.php';
+    require DOL_DOCUMENT_ROOT . '/core/tpl/admin_extrafields_edit.tpl.php';
 }
 
 llxFooter();
