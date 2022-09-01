54333c2496e9550449d5fc9548205ea764a61333
mikygee <mikydevel@yahoo.fr>
2022-09-01 23:30:33 +0200

    Corrections de liens morts immoproperty_document.php vers back to list

--- property/immoproperty_document.php
+++ property/immoproperty_document.php
@@ -130,7 +130,7 @@ if ($object->id) {
 
 	// Object card
 	// ------------------------------------------------------------
-	$linkback = '<a href="' . dol_buildpath('/ultimateimmo/immoproperty_list.php', 1) . '?restore_lastsearch_values=1' . (!empty($socid) ? '&socid=' . $socid : '') . '">' . $langs->trans("BackToList") . '</a>';
+	$linkback = '<a href="' . dol_buildpath('/ultimateimmo/property/immoproperty_list.php', 1) . '?restore_lastsearch_values=1' . (!empty($socid) ? '&socid=' . $socid : '') . '">' . $langs->trans("BackToList") . '</a>';
 
 	$morehtmlref = '<div class="refidno">';
 	// Ref owner
