d172fee210326f48c8b2d204c26e622e244d1418
Florian HENRY <florian.henry@open-concept.pro>
2025-08-21 16:26:45 +0200

    fix: php8 warning

--- renter/immorenter_agenda.php
+++ renter/immorenter_agenda.php
@@ -106,7 +106,7 @@ if ($id > 0 || !empty($ref)) {
  *	Actions
  */
 
-$parameters = array('id' => $socid);
+$parameters = array();
 $reshook = $hookmanager->executeHooks('doActions', $parameters, $object, $action);    // Note that $action and $object may have been modified by some hooks
 if ($reshook < 0) setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
 
